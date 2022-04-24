85ede158c50816b9cb316f230fcd43932422640f
Tiago <ztiagok@gmail.com>
2022-04-24 12:48:28 -0500

    c4 - Requisito 18 completo

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ function generateLetter()
     const elementParagraph = document.getElementById('carta-gerada')
     const elementInput = document.querySelector('input')
     const splittedValue = elementValue.split(' ')
+    const elementCounter = document.querySelector('#carta-contador')
     const styles = ['newspaper', 'magazine1', 'magazine2', 'medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright']
+
 
     if (elementInput.value.trim().length === 0)
     {
@@ -22,21 +24,45 @@ function generateLetter()
         for (let index = 0; index < splittedValue.length; index += 1)
         {
 
-            elementParagraph.innerHTML += '<span>' + splittedValue[index] + '</span>'     
-            elementParagraph.children[index].className = styles[Math.floor(Math.random() * styles.length)]
-        }   
-    }
+            let randomizer = styles[Math.floor(Math.random() * styles.length)] 
 
-    
+            elementParagraph.innerHTML += '<span>' + splittedValue[index] + '</span>'     
+            elementParagraph.children[index].className = randomizer
+        }   
+
+        elementCounter.innerHTML = splittedValue.length
+
+        let click = document.querySelectorAll('span')
+
+        for (let index = 0; index < click.length; index += 1)
+        {
+            click[index].addEventListener('click', randomizeLetter)
+        }
+    }
 }
 
+function randomizeLetter(origin)
+{
+
+    const styles = ['newspaper', 'magazine1', 'magazine2', 'medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright']
+    let randomizer = styles[Math.floor(Math.random() * styles.length)]
+
+    origin.target.className = randomizer
+    // const element = document.getElementById('carta-gerada').children
+
+    // for (let index = 0; index < element.length; index += 1)
+    // {
+    //      
+    //     element[index].className = randomizer
+    // }
+    
 
 
-
-
-// CALL FUNCTION
+}
 
 // EVENT LISTENERS
 
 let button = document.querySelector('#criar-carta')
+// let letter = document.querySelector('#carta-gerada')
 button.addEventListener('click', generateLetter)
+// letter.addEventListener('click', randomizeLetter)
